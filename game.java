39c539262b5082a25b86aa4e62439f27a38a7721
jeshurun olaide <sketchtitan7@gmail.com>
2025-02-22 09:33:57 -0800

    gamew

--- game.java
+++ game.java
@@ -12,28 +12,35 @@ public class game{
         int busB = 4;
         int eat = 1;
         if (pchoice == 1){
-            System.err.println("you chose busA ");
+            System.out.println("you chose busA ");
             time =time+ busA;
             System.out.println("you arrived halfway by "+ time+ " "+" oclock do you want to eat before you leave 1 for yes 2 for no");
         }else if (pchoice == 2){
-            System.err.println("you chose busB");
+            System.out.println("you chose busB");
             time = time + busB;
             System.out.println("you arrived halfway by "+ time + " " + " oclock do you want to eat before you leave 1 for yes, 2 for no");
         }
         //pchoice 
         pchoice = (int)(Math.random() * 2) + 1;
         if(pchoice == 1){
-            System.err.println("you chose to wait and eat. the time is"+ WasteTime(eat , time));
+            System.out.println("you chose to wait and eat. the time is"+ WasteTime(eat , time));
         }else if(pchoice == 2){
-            System.err.println("you chose to wait and eat. the time is"+ time);
+            System.out.println("you chose to wait and eat. the time is"+ time);
         }
-        
+         CheckTime(time);
 
- 
     }
     public static int WasteTime(int wasteType, int time){
         time = time + wasteType;
         return time;
+    }
+
+    public static void CheckTime(int time){
+        if(time >= 10){
+            System.out.println("you are late, you have missed the bus");
+        }else{
+            System.out.println("you made it on time");
+        }
     }
 
 }
