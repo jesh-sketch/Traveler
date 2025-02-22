27815ddece38d14a663342e3cf93e554c0cb5074
jesh-sketch <sketchtitan7@gmail.com>
2025-02-22 17:14:26 +0000

    Update base.js

--- base.js
+++ base.js
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     String name = ""
+    Scanner myObj = new Scanner(System.in);  
+    System.out.println("Please enter players name: ");
+    name = myObj.nextLine(); // Read user input
+
     
   }
 }
